3847111b7d001bede138819bda442cdd794c8e19
George King <kingg2@aston.ac.uk>
2016-02-18 00:16:26 +0000

    deleted wanky face

--- src/BrandonPlz.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Aaron on 18/02/2016.
- */
-public class BrandonPlz {
-    public BrandonPlz(){
-        System.out.println("Wanky Face");
-    }
-}
--- src/DeadCat.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Aaron on 18/02/2016.
- */
-public class DeadCat {
-
-    public DeadCat(){
-        System.out.println("Ha!");
-    }
-}
