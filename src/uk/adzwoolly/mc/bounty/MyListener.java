5126865ef1ed6594bb875166d945f40d7bc32c31
Adzwoolly <Adzwoolly@100.71.32.109>
2016-02-23 00:00:55 +0000

    Commiting because it says something has changed.  I don't know what I changed but, it's not broken so have it.

--- src/uk/adzwoolly/mc/bounty/MyListener.java
+++ src/uk/adzwoolly/mc/bounty/MyListener.java
@@ -2,7 +2,6 @@ package uk.adzwoolly.mc.bounty;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
