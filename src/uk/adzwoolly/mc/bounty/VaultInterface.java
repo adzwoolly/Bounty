862679e171823ecd3f03ad2c675a94da87434348
ALargeBagel <aaron.tkd@hotmail.com>
2016-03-03 23:19:41 +0000

    Might remove soon

--- src/uk/adzwoolly/mc/bounty/VaultInterface.java
+++ src/uk/adzwoolly/mc/bounty/VaultInterface.java
@@ -1,7 +1,25 @@
 package uk.adzwoolly.mc.bounty;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
 /**
  * Created by Aaron on 03/03/2016.
  */
 public class VaultInterface {
+
+    private Bounty bounty;
+    private RegisteredServiceProvider<Economy> economyProvider;
+    private Economy economy;
+
+    public VaultInterface(Bounty bounty){
+        bounty = this.bounty;
+        RegisteredServiceProvider<Economy> economyProvider = bounty.getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+        if (economyProvider != null) {
+            economy = economyProvider.getProvider();
+        } else{
+            bounty.getLogger().severe("There is no economy plugin installed.");
+        }
+
+    }
 }
